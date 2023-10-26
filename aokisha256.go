4d69c1aa3788639de26ffef4b68da697716b114b
yuki-js <me@aoki.app>
2023-10-26 17:07:52 +0900

    add doc

--- aokisha256.go
+++ aokisha256.go
@@ -2,6 +2,7 @@ package aokisha256
 
 import "github.com/PenisProtocol/aokisha256/internal"
 
+// Hash returns the SHA256 hash of the given byte slice.
 func Hash(target []byte) [32]byte {
 	return internal.Hash(target)
 }
